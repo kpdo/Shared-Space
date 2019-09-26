c953f92367825037fe66cc8054a6192554dfc0d4
Stephen Rogers <stephen.rogers6@gmail.com>
2019-09-26 18:20:30 -0500

    Added a scripts folder and started a script for importing objects

---
+++ webVrUnityProject/webVR/Assets/Scripts/ObjectImporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Uploads 3d models such as .obj into the scene from an sql database in JSON format.
+public class ObjectImporter : MonoBehaviour
+{
+
+    //Grabs the JSON object from a url and converts it into a Mesh
+    public Mesh GetObjectMesh(string url)
+    {
+        WWW web = new WWW(url);
+        string jsonString = web.text;
+
+        List<Vector3> vertices = JsonUtility.FromJson<List<Vector3>>(jsonString);
+
+
+
+        Mesh toReturn = new Mesh();
+        toReturn.SetVertices(vertices);
+
+        return toReturn;
+    }
+
+}
